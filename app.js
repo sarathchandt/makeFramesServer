bf86d2761d1f74056b8482e4225b79cad23298c2
sarathchandt <sarathchand770@gmail.com>
2023-03-07 18:30:03 +0530

    before review

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true,  parameterLimit: 50000 })); 
 app.use(express.static(path.join(__dirname, 'public'))); 
 app.use(cors({ 
-    origin:['https://makeframes.netlify.app', 'http://localhost:8001'],
+    origin:['https://makeframes.netlify.app'],
     method:['POST', 'GET', 'PUT', 'DELETE','PATCH'],
     credentials: true,
     allowedHeaders: [
@@ -66,8 +66,8 @@ app.use('/admin',adminRoutes)
 
 const io = new SocketIOServer(server, {
     cors: {
-        // origin:'https://makeframes.netlify.app',
-        origin:'http://localhost:8001',
+        origin:'https://makeframes.netlify.app',
+        // origin:'http://localhost:8001',
 
         methods:['POST', 'GET'],
         credentials: true,
