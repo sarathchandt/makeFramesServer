0f43489d00727d0817405bdddf6aa71a71f23082
sarathchandt <sarathchand770@gmail.com>
2023-03-07 10:19:55 +0530

    changed token

--- middleware/jsonWTMiddleWare.mjs
+++ middleware/jsonWTMiddleWare.mjs
@@ -29,16 +29,16 @@ export function verifyToken(req, res, next) {
 }
 
 export function verifyTokenHeader(req, res, next) {
-
-    if (!req.headers.cookie ) {
+    let token = req?.headers?.authorization?.split(' ')[1]
+    if (token == 'null' ) {
         res.status(400).json({ token: false })
     } else {
-        jwt.verify(req.headers.cookie, process.env.JWT_SECRET, (err, data) => {
+        jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
             if (err) {
                 console.log(err);
                 res.status(400).json({ token: false })
             } else {
-                res.locals.userId = data.email
+                res.locals.userId = data?.email
                 next()
             }
         })
