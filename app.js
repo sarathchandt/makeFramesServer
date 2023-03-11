78453f5d6d6acbda297bfe5d54dc5d8be652ba72
sarathchandt <sarathchand770@gmail.com>
2023-03-11 15:55:41 +0530

    url

--- app.js
+++ app.js
@@ -36,11 +36,11 @@ app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true,  parameterLimit: 50000 })); 
 app.use(express.static(path.join(__dirname, 'public'))); 
 app.use(cors({ 
-//     origin:['https://makeframes.netlify.app',
+    origin:'https://makeframes.netlify.app',
   
 //     "https://main.d1me12wsnvmr92.amplifyapp.com/" 
 // ],
- origin:  "http://localhost:8000"  ,
+//  origin:  "http://localhost:8000"  ,
     method:['POST', 'GET', 'PUT', 'DELETE','PATCH'],
     credentials: true,
     allowedHeaders: [
@@ -70,9 +70,9 @@ app.use('/admin',adminRoutes)
 
 const io = new SocketIOServer(server, {
     cors: {
-        // origin:'https://makeframes.netlify.app',
+        origin:'https://makeframes.netlify.app',
         
-        origin:'http://localhost:8000',
+        // origin:'http://localhost:8000',
         // origin:'*',
 
         methods:['POST', 'GET', 'PUT', 'DELETE','PATCH'],
