2112daca3a6661008c6ec5da6268031ec8f36db3
sarathchandt <sarathchand770@gmail.com>
2023-05-29 19:14:52 +0530

    changed nodemailer to emailjs

--- nodeMailer/nodeMailer.mjs
+++ nodeMailer/nodeMailer.mjs
@@ -1,5 +1,6 @@
 
 import nodemailer from 'nodemailer'
+import { SMTPClient } from 'emailjs';
 
 
 var otp = Math.random();
@@ -8,51 +9,40 @@ otp = parseInt(otp);
 
 
 export const sendOtpMessage = (email) => {
-    return new Promise((resolve, reject) => {
-console.log(otp);
-        let mailOptions = {
-            to: email,
-            subject: 'otp for makeframe Login',
-            html: `<h2> Your OTP from makeframes is  : ${otp} </h2>`
-        }
+    return new Promise(async(resolve, reject) => {
+        console.log(otp);
 
-
-        let transporter = nodemailer.createTransport({
+        const client = new SMTPClient({
+            user: process.env.EMAIL,
+            password: process.env.PASSWORD,
             host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD
-            } 
-        })
+            ssl: true,
+        });
 
-        transporter.sendMail(mailOptions).then((res) => {
-            resolve({ messaged: true })
-            console.log("Message sent: %s", res.messageId);
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(res));
-            return ({ messaged: true })
-
-        }).catch(err => {
-
-            resolve({ messaged: false })
-            console.log(err);
-            return ({ messaged: false })
-        })
-
-
+        try {
+            const message = await client.sendAsync({
+                text: ` Your OTP from makeframes is  : ${otp} `,
+                from: 'makeframes2023@gmail.com',
+                to: `${email}`,
+                // cc: 'else <else@your-email.com>',
+                subject: 'OTP VERIFICATION',
+            });
+            console.log(message);
+        } catch (err) {
+            console.error(err);
+        }
     })
 
 }
 
-export const verifyOtp=(clientOtp)=>{
+export const verifyOtp = (clientOtp) => {
 
-    return new Promise((resolve, reject)=>{
-     
-        if(Number(clientOtp) === otp){
-            resolve({otp:true})
-        }else{
-            resolve({otp:false})
+    return new Promise((resolve, reject) => {
+
+        if (Number(clientOtp) === otp) {
+            resolve({ otp: true })
+        } else {
+            resolve({ otp: false })
         }
     })
 }
